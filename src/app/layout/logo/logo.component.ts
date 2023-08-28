7436bab172eeb737da5d54de4b4fa707f2dbb196
Sil-Cro-Dev <s.paloni@solvingteam.it>
2023-08-28 19:51:37 +0200

    fix grafico

--- src/app/layout/logo/logo.component.ts
+++ src/app/layout/logo/logo.component.ts
@@ -9,10 +9,13 @@ export class LogoComponent implements OnInit, OnChanges {
   @Input() isOpened: boolean = false;
   logo: HTMLElement | null = null;
   logoImg: HTMLElement | null = null;
+  parentLogo: HTMLElement | null = null;
+
 
   ngOnInit() {
     this.logo = document.getElementById("logo");
-    this.logoImg = document.getElementById("logoImg");
+    this.logoImg = document.getElementById("logo-img");
+    this.parentLogo = document.getElementById("parent-logo");
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -20,8 +23,23 @@ export class LogoComponent implements OnInit, OnChanges {
   }
 
   changeAttributes() {
+    this.changeAttributeParentLogo();
     this.changeAttributeLogo();
     this.changeAttributeLogoImg();
+  }
+
+  changeAttributeParentLogo() {
+
+    if (this.parentLogo) {
+      if (!this.isOpened) {
+        this.parentLogo.classList.remove("bg-purple-400/90", "grayscale-0", "rounded-lg");
+        this.parentLogo.classList.add("hover:grayscale-0", "hover:bg-purple-300");
+      } else {
+        this.parentLogo.classList.remove("hover:grayscale-0", "hover:bg-purple-300");
+        this.parentLogo.classList.add("bg-purple-400/90", "grayscale-0", "rounded-lg");
+      }
+    }
+
   }
 
   changeAttributeLogo() {
--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -26,13 +26,13 @@ export class MenuComponent implements OnInit, OnChanges {
     if (this.elementLists) {
       if (!this.isOpened) {
         for (let i = 0; i < this.elementLists.length; i++) {
-          this.elementLists[i].classList.remove("sm:justify-start", "sm:px-4", "border-t", "border-neutral-500/60", "last:border-b")
-          this.elementLists[i].classList.add("sm:justify-center")
+          this.elementLists[i].classList.remove("sm:justify-start", "sm:px-4", "sm:border-t", "border-neutral-500/60", "sm:last:border-b-0", "sm:last:rounded-b-lg", "sm:first:border-t-0", "sm:first:rounded-t-lg")
+          this.elementLists[i].classList.add("sm:justify-center", "rounded-lg")
         }
       } else {
         for (let i = 0; i < this.elementLists.length; i++) {
-          this.elementLists[i].classList.remove("sm:justify-center")
-          this.elementLists[i].classList.add("sm:justify-start", "sm:px-4", "border-t", "border-neutral-500/60", "last:border-b")
+          this.elementLists[i].classList.remove("sm:justify-center", "rounded-lg")
+          this.elementLists[i].classList.add("sm:justify-start", "sm:px-4", "sm:border-t", "border-neutral-500/60", "sm:last:border-b-0", "sm:last:rounded-b-lg", "sm:first:border-t-0", "sm:first:rounded-t-lg")
         }
       }
     }
